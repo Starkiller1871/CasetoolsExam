44403251eb391e84255c2e12addead2471614b4a
exam <exam@Bcalab4-m02>
2023-05-04 12:11:23 +0530

    code for piggybank

---
+++ CaseToolsExam/src/PiggyBank_225.java
@@ -0,0 +1,16 @@
+
+public class PiggyBank_225 {
+	int num_coins;
+	int coin_value;
+	PiggyBank_225()
+	{
+		this.num_coins=num_coins;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
